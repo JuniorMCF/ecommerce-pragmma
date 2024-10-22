d984f3f95fc6967b064ebacbb882e17fe3e3ada7
JuniorMCF <juniorcastillon6@gmail.com>
2024-10-21 20:47:08 -0500

    package json dev dependency bycript

--- src/shared/bindings/category.binding.ts
+++ src/shared/bindings/category.binding.ts
@@ -4,11 +4,8 @@ import { CategoryController } from "../../categoy.module/presentation/controller
 import { CreateCategoryCommand } from "../../categoy.module/application/commands/create-category.command";
 import { GetCategoryByIdQuery } from "../../categoy.module/application/queries/get-category-by-id.query";
 import { DeleteCategoryCommand } from "../../categoy.module/application/commands/delete-category.command";
-import { ICategoryService } from "../../categoy.module/application/services/icategory.service";
 import { CategoryService } from "../../categoy.module/infraestructure/services/category.service";
-
 import { UpdateCategoryCommand } from "../../categoy.module/application/commands/update-cateogy.command";
-import { ICategoryRepository } from "../../categoy.module/domain/repositories/icategory.repository";
 import { MongoCategoryRepository } from "../../categoy.module/infraestructure/repositories/mongo-category.repository";
 
 
